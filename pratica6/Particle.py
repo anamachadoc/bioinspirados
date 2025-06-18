f1961cdd66411b1f0636c02205bc6fd0ad48eaf9
anamachadoc <anaclaudiamachado211@gmail.com>
2025-06-18 13:10:38 -0300

    particle completed

--- pratica6/Particle.py
+++ pratica6/Particle.py
@@ -2,19 +2,39 @@ from typing import Any, Dict
 import os
 import random
 import numpy as np
+import math
 
 class Particle():
     def __init__(self, position: np.ndarray, **kwargs: Dict[str, Any]):
         for key, value in kwargs.items():
             setattr(self, key, value)
         self.position = position
-        self.speed = 0
+        self.pbest = position
+        self.speed = np.zeros(self.num_dimensions)
+        self.fitness = self.function_fitness()
 
     def get_position(self):
         return self.position
     
     def get_speed(self):
         return self.speed
+
+    def get_fitness(self):
+        return self.fitness
     
-    def new_speed(self):
+    def _new_speed(self, gbest):
         r1, r2 = random.random(), random.random()
+        diversity = self.w * self.speed
+        individual_intensification = self.c1 * r1 * (self.pbest - self.position)
+        social_intensification = self.c2 * r2 * (gbest - self.position)
+        return diversity + individual_intensification + social_intensification
+    
+    def _new_position(self):
+        return self.position + self.speed
+    
+    def function_fitness(self):
+        return -20 * pow(math.e, (-0.2 * math.sqrt(1/self.num_dimensions * sum(x**2 for x in self.position)))) - pow(math.e, 1/self.num_dimensions * sum(math.cos(2 * math.pi * x) for x in self.position)) + 20 + math.e
+    
+    def att_particle(self):
+        self._new_speed()
+        self._new_position()
--- pratica6/ParticleSwarm.py
+++ pratica6/ParticleSwarm.py
@@ -1,9 +1,18 @@
 from Particle import Particle 
 from typing import Any, Dict
 import os
+import numpy as np
 
 class ParticleSwarm():
     def __init__(self, **kwargs: Dict[str, Any]):
         for key, value in kwargs.items():
             setattr(self, key, value)
         os.makedirs(self.output_path, exist_ok=True)
+        self.particles = [Particle(np.random.uniform(low=self.x_limits[0], high=self.x_limits[1], size=self.num_dimensions), **kwargs)
+        for p in range(self.num_pop)]
+        
+    def get_population(self):
+        return self.particles
+    
+    def get_gbest(self):
+        
