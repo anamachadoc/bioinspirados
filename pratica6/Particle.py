41df652c0d0f825fe4920411f460c215711791f7
anamachadoc <anaclaudiamachado211@gmail.com>
2025-06-23 09:21:43 -0300

    first version completed

--- pratica6/Particle.py
+++ pratica6/Particle.py
@@ -10,8 +10,10 @@ class Particle():
             setattr(self, key, value)
         self.position = position
         self.pbest = position
+        self.gbest = None
         self.speed = np.zeros(self.num_dimensions)
-        self.fitness = self.function_fitness()
+        self.fitness = self.function_fitness(self.position)
+        self.neighbors = []
 
     def get_position(self):
         return self.position
@@ -22,19 +24,45 @@ class Particle():
     def get_fitness(self):
         return self.fitness
     
-    def _new_speed(self, gbest):
+    def get_neighbors(self):
+        return self.neighbors
+    
+    def get_gbest(self):
+        return self.gbest
+    
+    def get_pbest(self):
+        return self.pbest
+    
+    def set_neighbors(self, neighbors):
+        self.neighbors = neighbors
+    
+    def set_pbest(self):
+        if self.function_fitness(self.position) < self.function_fitness(self.pbest):
+            self.pbest = self.position
+        
+    def set_gbest(self):
+        best_neighbor = min(self.neighbors, key=lambda p: p.get_fitness())
+        if self.gbest is None or best_neighbor.get_fitness() < self.function_fitness(self.gbest):
+            #if self.gbest is not None:
+                #print(f'de {self.function_fitness(self.gbest)} para {best_neighbor.get_fitness()}')
+            self.gbest = best_neighbor.get_position()
+    
+    def _new_speed(self):
+        if self.gbest is None:
+            return
         r1, r2 = random.random(), random.random()
         diversity = self.w * self.speed
         individual_intensification = self.c1 * r1 * (self.pbest - self.position)
-        social_intensification = self.c2 * r2 * (gbest - self.position)
-        return diversity + individual_intensification + social_intensification
+        social_intensification = self.c2 * r2 * (self.gbest - self.position)
+        self.speed = diversity + individual_intensification + social_intensification
     
     def _new_position(self):
-        return self.position + self.speed
+        self.position = self.position + self.speed
     
-    def function_fitness(self):
-        return -20 * pow(math.e, (-0.2 * math.sqrt(1/self.num_dimensions * sum(x**2 for x in self.position)))) - pow(math.e, 1/self.num_dimensions * sum(math.cos(2 * math.pi * x) for x in self.position)) + 20 + math.e
+    def function_fitness(self, position):
+        return -20 * pow(math.e, (-0.2 * math.sqrt(1/self.num_dimensions * sum(x**2 for x in position)))) - pow(math.e, 1/self.num_dimensions * sum(math.cos(2 * math.pi * x) for x in position)) + 20 + math.e
     
     def att_particle(self):
         self._new_speed()
         self._new_position()
+        self.fitness = self.function_fitness(self.position)
--- pratica6/ParticleSwarm.py
+++ pratica6/ParticleSwarm.py
@@ -10,9 +10,67 @@ class ParticleSwarm():
         os.makedirs(self.output_path, exist_ok=True)
         self.particles = [Particle(np.random.uniform(low=self.x_limits[0], high=self.x_limits[1], size=self.num_dimensions), **kwargs)
         for p in range(self.num_pop)]
+        self._define_neighborhood()
+        self._att_pbest()
+        self._att_gbest()
         
+    def _define_neighborhood(self):
+        if self.topology == 'global':
+            self._set_global_topology()
+        elif self.topology == 'ring':
+            self._set_ring_topology()
+        elif self.topology == 'focal':
+            self._set_focal_topology()
+        else:
+            raise ValueError(f"invalid topology: {self.topology}")
+
+    def _set_global_topology(self):
+        for particle in self.particles:
+            particle.set_neighbors(self.particles)
+
+    def _set_ring_topology(self):
+        for i, particle in enumerate(self.particles):
+            neighborhood = [
+                self.particles[(i - 1) % self.num_pop],
+                self.particles[(i + 1) % self.num_pop],
+                particle
+            ]
+            particle.set_neighbors(neighborhood)
+
+    def _set_focal_topology(self):
+        focal = min(self.particles, key=lambda p: p.get_fitness())
+        for particle in self.particles:
+            if particle is focal:
+                neighborhood = self.particles
+            else:
+                neighborhood = [focal]
+            particle.set_neighbors(neighborhood)
+
+    def _att_pbest(self):
+        for p in self.particles:
+            p.set_pbest()
+          
+    def _att_gbest(self):
+        for p in self.particles:
+            p.set_gbest()
+                
     def get_population(self):
-        return self.particles
+        return self.particles   
     
-    def get_gbest(self):
+    def get_gbest(self, particle):
+        return min(particle.get_neighbors(), key=lambda p: p.get_fitness())
+    
+    def get_best_solution(self):
+        p = min(self.particles, key=lambda p: p.function_fitness(p.get_gbest()))
+        return p.get_gbest(), p.function_fitness(p.get_gbest())
         
+    def run(self):
+        iteration = 0
+        while iteration < self.max_iteration:
+            for p in self.particles:
+                p.att_particle()
+            self._att_pbest()
+            self._att_gbest()
+            print(f'iteracao {iteration} - {self.get_best_solution()}')
+            self._define_neighborhood()
+            iteration += 1
